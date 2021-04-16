35c1a05cc1ce59afc28b6585ab549e8c96d000c1
SumitBaria <imsumitbaria@gmail.com>
2021-04-16 18:20:58 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ImageViewComponent } from './Components/image-view/image-view.component';
+import { SigninComponent } from './Components/signin/signin.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "signin", component: SigninComponent},
+  {path: "home", component: ImageViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { SigninComponent } from './Components/signin/signin.component';
+import { ImageViewComponent } from './Components/image-view/image-view.component';
+import { CardComponent } from './Core/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SignupComponent,
+    SigninComponent,
+
+    ImageViewComponent,
+     CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
